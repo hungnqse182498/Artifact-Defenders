e3cba269ab5f22879cf3c973def7f76d5d1e376e
duckhynh <phamduchung888888@gmail.com>
2025-10-16 01:47:01 +0700

    Thêm tiếng Việt và chỉnh UI menu

---
+++ Assets/Scripts/Localization/LocalizableString.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ScriptableObject chính dùng cho localization (đa ngôn ngữ)
+/// </summary>
+[CreateAssetMenu(fileName = "New string", menuName = "Loc. String")]
+public class LocalizableString : ScriptableObject
+{
+    [Header("Translations")]
+    public string english;
+    public string vietnamese;
+
+    /// <summary>
+    /// Trả về chuỗi tương ứng với ngôn ngữ được chọn
+    /// </summary>
+    public string GetString(Language language)
+    {
+        switch (language)
+        {
+            case Language.English:
+                return english;
+            case Language.Vietnamese:
+                return vietnamese;
+        }
+
+        return null;
+    }
+
+    [System.Serializable]
+    public enum Language { English, Vietnamese };
+}
